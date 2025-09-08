520cbc15054c6937281d306ac01b4ed8ca70f555
Lord-Y <Lord-Y@users.noreply.github.com>
2025-09-08 15:37:15 +0200

    tests(assert): Uncomment TestAssert

--- assert/assert_test.go
+++ assert/assert_test.go
@@ -3,6 +3,7 @@ package assert
 import (
 	"bytes"
 	"strings"
+	"testing"
 )
 
 type vv struct {
@@ -23,24 +24,24 @@ var alt = func() string {
 	return buf.String()
 }
 
-// func TestAssert(t *testing.T) {
-// 	var buf bytes.Buffer
-// 	stderr = &buf
-// 	exit = func(code int) {}
-// 	var myval bool
-// 	Assert((myval == true || true == false || "h(el)\"" == "hello"))
-// 	if !strings.HasPrefix(buf.String(), "Assertion failed:") {
-// 		t.Fatal("missing failed text")
-// 	}
-// 	if !strings.Contains(buf.String(),
-// 		`((myval == true || true == false || "h(el)\"" == "hello"))`) {
-// 		t.Fatal("missing condition text")
-// 	}
-// 	buf.Reset()
-// 	var v vv
-// 	v.do()
-// 	if !strings.Contains(buf.String(), `function vv.do`) {
-// 		t.Fatal("missing function")
-// 	}
-// 	alt()
-// }
+func TestAssert(t *testing.T) {
+	var buf bytes.Buffer
+	stderr = &buf
+	exit = func(code int) {}
+	var myval bool
+	Assert((myval == true || true == false || "h(el)\"" == "hello"))
+	if !strings.HasPrefix(buf.String(), "Assertion failed:") {
+		t.Fatal("missing failed text")
+	}
+	if !strings.Contains(buf.String(),
+		`((myval == true || true == false || "h(el)\"" == "hello"))`) {
+		t.Fatal("missing condition text")
+	}
+	buf.Reset()
+	var v vv
+	v.do()
+	if !strings.Contains(buf.String(), `function vv.do`) {
+		t.Fatal("missing function")
+	}
+	alt()
+}
